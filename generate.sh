843a4e06e04b7588e85880da96a3307034401b40
Jason Newman <jason@getkolla.com>
2022-11-15 17:33:23 +0000

    reorder check for swagger gen file

--- generate.sh
+++ generate.sh
@@ -2,15 +2,15 @@
 
 set -eux
 
-# remove the old generated files
-rm ./kc/swagger/model_*.go | true
-rm ./kc/swagger/docs/*.md | true
-
 # if swagger-codegen-cli.jar is not available, exit
 if [ ! -f swagger-codegen-cli.jar ]; then
     echo "swagger-codegen-cli.jar is not available. Please download with: wget https://repo1.maven.org/maven2/io/swagger/codegen/v3/swagger-codegen-cli/3.0.35/swagger-codegen-cli-3.0.35.jar -O ./swagger-codegen-cli.jar"
     exit 0
 fi
+
+# remove the old generated files
+rm ./kc/swagger/model_*.go | true
+rm ./kc/swagger/docs/*.md | true
 
 pushd kc/swagger
 java -jar ../../swagger-codegen-cli.jar generate -l go -i ./api/swagger.yaml --output ./
